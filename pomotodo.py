43269a9d55283cdf7cc5484e7e6634ae1d0b64e3
Vicki Yang <vwyangdev@gmail.com>
2022-05-07 18:13:23 -0400

    feat: completed pomo can be written to a CSV file with a description of the task

--- pomotodo.py
+++ pomotodo.py
@@ -1,9 +1,24 @@
 import time
 import math
+import csv
+
+
+def save_pomo(duration_min):
+    now = time.localtime()
+    fieldnames = ["date", "duration", "task"]
+    task = input("In a few words, what did you work on? ")
+    with open('data.csv', 'a', newline='') as f:
+        w = csv.DictWriter(f, fieldnames=fieldnames)
+
+        if f.tell() == 0:  # write header only if blank file
+            w.writeheader()
+
+        w.writerow({"date": f"{now.tm_mon}/{now.tm_mday}/{now.tm_year}",
+                    "duration": duration_min,
+                    "task": f"{task}"})
 
 
 def run_pomo(duration_min):
-    now = time.localtime()
     start_duration = duration_min * 60
     duration_s = start_duration
     time_to_add_block = math.ceil(duration_s / 50)
@@ -26,23 +41,25 @@ def run_pomo(duration_min):
         print(f'\r{m}:{lead_s}{s} {bar} {round(pct, 1)}% ', end="")
         time.sleep(1)
     print("\ndone!")
+    save_pomo(duration_min)
 
 
 def main_menu():
-    menu = {
-        "1": "25-min pomo",
-        "2": "15-min pomo"
-    }
+    while True:
+        menu = {
+            "1": "25-min pomo",
+            "2": "15-min pomo"
+        }
 
-    print("----------")
-    for item in menu:
-        print(f"{item}. {menu[item]}")
-    print("----------")
+        print("----------")
+        for item in menu:
+            print(f"{item}. {menu[item]}")
+        print("----------")
 
-    answer = input("What would you like to do? ")
+        answer = input("What would you like to do? ")
 
-    if answer == "1":
-        run_pomo(25)
+        if answer == "1":
+            run_pomo(25)
 
 
 main_menu()
